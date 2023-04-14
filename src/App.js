25cdb1e1f22d740f3163c50106136b714b81b72e
Will <darthhun54@gmail.com>
2023-04-14 12:47:39 -0400

    save the world

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload and you're way to go.
+          Edit <code>src/App.js</code> and save the world.
         </p>
         <a
           className="App-link"
